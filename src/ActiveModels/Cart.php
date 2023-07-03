4a5d7461c37a44cda932420ea513017f912b797f
Luc Princen <lucprincen@users.noreply.github.com>
2023-07-03 14:40:47 +0200

    added activemodels

---
+++ src/ActiveModels/Cart.php
@@ -0,0 +1,305 @@
+<?php
+
+    namespace BbqData\ActiveModels;
+
+    use Carbon\Carbon;
+    use BbqData\Models\User;
+    use BbqData\Models\Coupon;
+    use BbqData\Contracts\ActiveModel;
+    use Bbquality\Helpers\CarbonReduction;
+
+
+    class Cart extends ActiveModel{
+
+        /**
+         * Data object
+         *
+         * @var array
+         */
+        protected $data;
+
+
+        /**
+         * Constructor
+         *
+         * @param Int $cart_id
+         */
+        public function __construct( $cart_id )
+        {
+            $this->connect_collection();
+            $this->cart_id = $cart_id;
+            if( $this->valid_id( $cart_id ) ){
+                $this->data = $this->fetch_data( $cart_id );
+            }
+        }     
+        
+        /**
+         * Return the collection name for this active model
+         *
+         * @return string
+         */
+        public static function get_collection_name() 
+        {
+            return 'carts';   
+        }
+
+        /*====================================*/
+        /*          Getters                   */
+        /*====================================*/
+
+        /**
+         * Return any data in the data-array
+         *
+         * @return void
+         */
+        public function get( $key, $default = null )
+        {
+            if( isset( $this->data[ $key ] ) ){
+                return $this->data[ $key ];
+            }
+
+            return $default;
+        }
+
+        /**
+         * Return the subtotal without gift certificates
+         *
+         * @return void
+         */
+        public function get_subtotal_without_gift_certificates()
+        {
+            $subtotal = 0;
+            $certificates = Coupon::getCertificateIds();
+
+            foreach( $this->get('rows',[]) as $row ){
+
+                //if this row has no price:
+                if( !isset( $row['price'] ) || is_null( $row['price'] ) ){
+                    continue;
+                }
+
+                //if it's a gift certificate: 
+                if( in_array( $row['id'], $certificates ) ){
+                    continue;
+                }
+
+                //if it's charity:
+                if( $row['id'] == CarbonReduction::getProduct() ){
+                    continue;
+                }
+                
+                //else, add it to the subtotal
+                $subtotal += ( $row['price'] * $row['quantity'] );
+            }
+
+            return $subtotal;
+        }
+
+
+        /**
+         * Calculate over which amount discounts can be calculated
+         *
+         * @return void
+         */
+        public function get_discount_applicable_subtotal()
+        {
+            $subtotal = $this->get_subtotal_without_gift_certificates();
+
+            //loop through rows:
+            foreach( $this->get('rows', []) as $row ){
+
+                //check if the row has a discount_type:
+                if( is_null( $row['discount_type'] ) ){
+                    continue;   
+                }
+                
+                //if it's in sale
+                $subtotal -= ( $row['price'] * $row['quantity'] );
+
+            }
+                
+            return $subtotal;
+        }
+
+
+        /**
+         * Check if the content of this cart is just certificates
+         *
+         * @return bool
+         */
+        public function only_has_certificates()
+        {
+            $certificate_ids = Coupon::getCertificateIds();
+
+            //add the carbon product, because it doesn't count as a cart item:
+            $certificate_ids[] = CarbonReduction::getProduct();
+            
+            foreach( $this->get('rows', []) as $row ){
+                if( !in_array( $row['id'], $certificate_ids ) ){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /**
+         * A check to see if a cart has free shipping:
+         *
+         * @return boolean
+         */
+        public function has_free_shipping()
+        {
+            //check subtotal:
+            //@todo put 75 in a Shipping helper
+            if( $this->get_subtotal_without_gift_certificates() >= 75 ){
+                return true;
+            }
+
+            //check coupons:
+            foreach( $this->get('discounts', []) as $discount ){
+                if( $discount['free_shipping'] || $discount['free_shipping'] == 1 ){
+                    return true;
+                }
+            }
+
+            //check product:
+            $product_ids = explode( ',', env('free_shipping_products') );
+
+            foreach( $this->get('rows',[]) as $row ){
+                if( in_array( $row['id'], $product_ids ) ){
+                    return true;
+                }
+            }
+
+            //check membership:
+            if( !is_null( User::current() ) && !is_null( User::current()->membership ) ){
+                return true; 
+            }
+
+            if( Carbon::now() > '2023-03-03 23:59:59' && Carbon::now() < '2023-03-06 00:00:00') {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        /*====================================*/
+        /*          Discounts                 */
+        /*====================================*/
+
+
+        /**
+         * Attach a coupon to this cart
+         *
+         * @param Coupon $coupon
+         * @return void
+         */
+        public function add_discount( $coupon )
+        {
+            $data = $this->data;
+            $discounts = $this->get( 'discounts', [] );
+
+            //setup the new discount:
+            $discounts[] = [
+                'id' => $coupon->id,
+                'code' => $coupon->code,
+                'amount' => $coupon->amount,
+                'type' => $coupon->type,
+                'free_shipping' => $coupon->free_shipping,
+                'gift_certificate' => $coupon->is_gift_certificate,
+                'coupon_campaign_id' => $coupon->coupon_campaign_id,
+            ];
+
+            //add it as a json to the order:
+            $data['discounts'] = $discounts;
+            $this->save( $data );
+        }
+
+
+        /**
+         * Remove a discount
+         *
+         * @param Coupon $coupon
+         * @return void
+         */
+        public function remove_discount( $coupon )
+        {
+            $data = $this->data;
+            $discounts = $this->get('discounts', [] );
+            $new_discounts = [];
+            foreach( $discounts as $discount ){
+                if( $discount['code'] !== $coupon->code ){
+                    $new_discounts[] = $discount;
+                }
+            }
+
+            $data['discounts'] = $new_discounts;
+            $this->save( $data );
+        }
+
+
+
+        /*====================================*/
+        /*          Helper functions          */
+        /*====================================*/
+
+
+        /**
+         * Save this cart with fresh data
+         *
+         * @param Array $data
+         * @return void
+         */
+        public function save( $data )
+        {
+            //always check for free shipping:
+            $data['delete_after'] = static::delete_after();
+
+            //set the data, so we can use it later.
+            $this->data = $data;
+
+            //and save the cart
+            return $this->save_data( $this->cart_id, $data );
+        }
+
+        /**
+         * Does this cart exist?
+         *
+         * @return boolean
+         */
+        public function exists()
+        {
+            return ( empty( $this->data ) ? false : true );
+        }
+
+        /**
+         * Create a new cart in Mongo,
+         * Send back the ID:
+         */
+        public static function create() 
+        {
+            $result = static::get_collection( 'carts' )->insertOne([
+                'order_id' => 0,
+                'rows' => [],
+                'analytics' => [],
+                'discounts' => [],
+                'delete_after' => static::delete_after()
+            ]);
+
+            return (string) $result->getInsertedId();
+        }   
+
+        /**
+         * Get the delete after date
+         *
+         * @return int (timestamp)
+         */
+        public static function delete_after()
+        {
+            return Carbon::now()->addWeeks( 2 )->timestamp;   
+        }
+
+       
+    }
---
+++ src/Contracts/ActiveModel.php
@@ -0,0 +1,164 @@
+<?php
+
+    namespace BbqData\Contracts;
+
+    use MongoDB\Client;
+    use MongoDB\BSON\ObjectId;
+
+    abstract class ActiveModel{
+
+        /**
+         * MongoDB Collection
+         *
+         * @var MongoCollection
+         */
+        protected $collection;
+
+        
+        /**
+         * Save a cart in MongoDB
+         *
+         * @param int $object_id
+         * @param array $data
+         * @return array
+         */
+        public function save_data( $object_id, $data )
+        {            
+            $cart = $this->fetch_data( $object_id );
+            if( !is_null( $cart ) ){
+                $this->collection->updateOne([ '_id' => new ObjectId( $object_id ) ], ['$set' => $data ]);
+            }
+        }
+
+        /**
+         * Fetch the mongo cart:
+         *
+         * @param int $object_id
+         * @return array
+         */
+        public function fetch_data( $object_id )
+        {
+            $result = $this->collection->findOne(['_id' => new ObjectId( $object_id ) ]);
+            if( !is_null( $result ) ){
+                return iterator_to_array( $result );
+            }
+
+            return null;
+        }
+
+
+        /**
+         * Find a single object
+         *
+         * @param array $query
+         * @return array
+         */
+        public static function find( $query )
+        {
+            $result = self::get_collection()->findOne( $query );
+            if( !is_null( $result ) ){
+                return iterator_to_array( $result );
+            }
+
+            return null;
+        }
+
+
+        /**
+         * Query a collection to receive an array of results    
+         *
+         * @param array $query
+         * @return array
+         */
+        public function query( $query )
+        {
+            $result = $this->collection->find( $query );
+            if( !is_null( $result ) ){
+                return iterator_to_array( $result );
+            }
+
+            return [];
+        }
+
+
+        /**
+         * Delete the cart
+         *
+         * @param int $order_id
+         * @return bool
+         */
+        public static function delete( $query )
+        {
+            $result = self::get_collection()->deleteOne( $query );
+            $count = $result->getDeletedCount();
+            return ( $count === 1 );
+        }
+
+
+        /**
+         * Check if this is a valid id
+         *
+         * @param int $id
+         * @return boolean
+         */
+        public function valid_id( $id )
+        {
+            try{
+                
+                $id = new ObjectId( $id );
+                return true;
+
+            }catch( \Throwable $e ){
+                return false;
+            }
+        }
+
+        /**
+         * Set the mongoDB connection
+         *
+         * @return void
+         */
+        public function connect_collection( $name = null )
+        {
+            //set the collection:
+            $this->collection = self::get_collection( $name );
+
+            //return this instance so we can chain commands:
+            return $this;
+        }
+
+        /**
+         * Get the mongo connection, statically
+         *
+         * @return MongoCollection
+         */
+        public static function get_collection( $name = null ) 
+        {
+            $db = env( 'mongo_db_name' );
+            $client = new Client( env('mongo_url' ) );
+
+            if( is_null( $name ) ){
+                $name =  static::get_collection_name();
+            }
+
+            //find the collection
+            $collection = $client->selectCollection( $db, $name );
+        
+            //return it:
+            return $collection;
+        }
+
+
+        /**
+         * Return the default collection name
+         *
+         * @return void
+         */
+        public static function get_collection_name() 
+        {
+            return 'items';    
+        }
+
+
+        
+    }
