ed3842333ea35b74e4745b5ffccb7f348b0d900d
Luc Princen <lucprincen@users.noreply.github.com>
2024-09-17 13:19:57 +0200

    adds start_on and end_on attributes as Carbon instances. Adds better scopes and allows for projects to query promotions based on threshold.

--- src/Models/Promotion.php
+++ src/Models/Promotion.php
@@ -2,6 +2,7 @@
 
     namespace BbqData\Models;
 
+	use Carbon\Carbon;
     use BbqData\Contracts\Model;
 
     class Promotion extends Model{
@@ -38,7 +39,75 @@
          */
         public function variation()
         {
-            return $this->hasOne( ProductVariation::class );
+            return $this->belongsTo( ProductVariation::class );
         }
 
+		
+		/**
+		 * Return the promotions for a certain threshold
+		 *
+		 * @param float $threshold
+		 * @return Promotion
+		 */
+		public static function forThreshold( $queried_threshold )
+		{
+			$response = \collect([]);
+			$results = static::current()->where('threshold', '<=', $queried_threshold )->ascending()->get();
+			if( !$results->isEmpty() ){
+				$results = $results->groupBy( 'threshold' );
+		
+				foreach( $results as $threshold => $promotions){
+					if( $queried_threshold >= $threshold ){
+						$response = $promotions;
+					}
+				}
+			}
+
+			return $response;
+		}
+
+		/**
+		 * Return the start on attribute as a carbon instance
+		 *
+		 * @param datetime $value
+		 * @return Carbon
+		 */
+		public function getStartOnAttribute( $value )
+		{
+			return \Carbon\Carbon::parse( $value );
+		}
+
+		/**
+		 * Return the end_on attribute as a carbon instance
+		 *
+		 * @param datetime $value
+		 * @return Carbon
+		 */
+		public function getEndOnAttribute( $value )
+		{
+			return \Carbon\Carbon::parse( $value );
+		}
+
+		/**
+		 * Only return current promotions
+		 *
+		 * @param QueryBuilder $builder
+		 * @return QueryBuilder
+		 */
+		public function scopeCurrent( $builder )
+		{
+			return $builder->where( 'start_on', '<=', Carbon::now() )->where( 'end_on', '>', Carbon::now() );
+		}
+
+		/**
+		 * Return promotions in ascending order
+		 *
+		 * @param QueryBuilder $builder
+		 * @return QueryBuilder
+		 */
+		public function scopeAscending( $builder )
+		{
+			return $builder->orderBy( 'threshold' );
+		}
+		
     }
